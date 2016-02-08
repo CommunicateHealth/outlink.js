424ed7f48d3179c6a2eee9f0cb4f70a49d973842
Jason Morris <jasonmorris@gmail.com>
2016-02-07 23:20:02 -0500

    Add grunt task for JS linting

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+
+module.exports = function(grunt) {
+
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // JSHint - Error check javascript
+    jshint: {
+      dist: {
+        options: {
+          curly: true,
+          eqeqeq: true,
+          eqnull: true,
+          browser: true,
+          globals: {
+            jQuery: true
+          }
+        },
+        files: {
+          src: ['Gruntfile.js', 'outlink.js']
+        }
+      }
+    },
+
+    // Connect - start local webserver
+    connect: {
+      server: {
+        options: {
+          port: 4000,
+          base: '.',
+          hostname: '127.0.0.1',
+          keepalive: true
+        }
+      }
+    },
+
+    // Update - find new versions of Grunt dependencies
+    devUpdate: {
+      main: {
+        options: {
+          updateType: 'prompt',
+          reportUpdated: false,
+          semver: false,
+          packages: {
+            devDependencies: true,
+            dependencies: false
+          },
+          packageJson: null
+        }
+      }
+    }
+
+  });
+
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+
+  // grunt - runs js, then serve tasks (see below)
+  grunt.registerTask('default', ['connect']);
+
+  // grunt js - Error checks, concatenation, minify JS
+  grunt.registerTask('js', ['jshint']);
+
+  // grunt update - Find new versions of Grunt libraries
+  grunt.registerTask('update', ['devUpdate']);
+
+};
