23eecc1e436ea9a10b4d427ef75954b83c3b0313
Jason Morris <jasonmorris@gmail.com>
2016-05-06 15:45:01 -0400

    Add rel="noopener noreferrer"
    As mentioned here:
    https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ External link handling
 * Alt and title attributes pull in current domain name (with fallback)
 * External sites links open in new window/tab **with** disclaimer and icon.
 * External .gov links to open in new window/tab **without** disclaimer or icon.
+* Added ```rel="noopener noreferrer"``` to prevent external site from manipulating ```window.opener``` object.
 
 ## Demo page
 
--- outlink.js
+++ outlink.js
@@ -10,12 +10,18 @@ var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; ++i) {
   // if URL contains .gov and doesn't contain current site URL
   if (links[i].href.indexOf(".gov") > -1 && links[i].href.indexOf(site) === -1 && links[i].className.indexOf("outlink-ignore")) {
-    links[i].setAttribute("target", "_blank");
+    links[i].setAttribute({
+      "target": "_blank",
+      "rel": "noopener noreferrer"
+    });
   }
 
   // if URL contains http(s) and doesn't contain current site URL
   else if ((links[i].href.indexOf("http://") > -1 || links[i].href.indexOf("https://") > -1) && links[i].href.indexOf(site) === -1 && links[i].className.indexOf("outlink-ignore")) {
-    links[i].setAttribute("target", "_blank");
+    links[i].setAttribute({
+      "target": "_blank",
+      "rel": "noopener noreferrer"
+    });
     links[i].innerHTML += "<img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAQCAQAAACIaFaMAAAAPUlEQVR4AWMgHUz4D4FgdgOQBYVIEggphATzBEYMCZhqrEahCTcAIRaJBggb3agGFOdi8wJZEpgQvwTJAADRrod38NnIHQAAAABJRU5ErkJggg==\" style=\"padding-left: .25em;\" alt=\"External Link: You are leaving "+ site +"\" title=\"External Link: You are leaving "+ site +"\">";
   }
 }
